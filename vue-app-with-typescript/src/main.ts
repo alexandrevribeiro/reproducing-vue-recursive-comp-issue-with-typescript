adb3f8456202e0ae787c3d01b589da707b4845a4
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-09-15 01:14:03 -0300

    Error reprodution (also when registering the comps globally)

--- vue-app-with-typescript/src/main.ts
+++ vue-app-with-typescript/src/main.ts
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/*
+-> ERROR ALSO OCCURRING WHEN REGISTERING THE COMPS GLOBALLY
+
+import Comp1 from './components/Comp1.vue'
+import Comp2 from './components/Comp2.vue'
+import Comp3 from './components/Comp3.vue'
+
+Vue.component('comp-1', Comp1);
+Vue.component('comp-2', Comp2);
+Vue.component('comp-3', Comp3);
+*/
+
 new Vue({
   el: '#app',
   render: h => h(App)
